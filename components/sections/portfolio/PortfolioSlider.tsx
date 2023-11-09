b0cf57ed678dec3bf542d6a0c93da651df2abd55
Pásztor Bence <pasztorbencee@gmai.com>
2023-11-09 14:08:15 +0100

    feat: animate portfolio slider on entry

--- components/sections/portfolio/PortfolioSlider.tsx
+++ components/sections/portfolio/PortfolioSlider.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { motion } from "framer-motion"
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -8,26 +9,43 @@ import Image from 'next/image';
 import { PORTFOLIO_IMAGES } from '@/lib/constants';
 
 const PortfolioSlider = () => {
+
+    const portfolioVariant = {
+        hidden: {
+            opacity: 0,
+            x: -42
+        },
+        visible: {
+            opacity: 1,
+            x: 0,
+            transition: {
+                duration: 0.4,
+            }
+        }
+    }
+
     return (
-        <Swiper
-            effect={'coverflow'}
-            grabCursor={true}
-            centeredSlides={true}
-            loop={true}
-            slidesPerView={'auto'}
-            coverflowEffect={{
-                rotate: 0,
-                stretch: 0,
-                modifier: 2,
-            }}
-            modules={[EffectCoverflow]}
-        >
-            {PORTFOLIO_IMAGES.map(({ src, alt }, index) => (
-                <SwiperSlide key={index} className="lg:!w-96 !w-64 aspect-square">
-                    <Image className="object-cover w-full h-full" src={src} alt={alt} />
-                </SwiperSlide>
-            ))}
-        </Swiper>
+        <motion.div variants={portfolioVariant} initial="hidden" whileInView="visible" viewport={{ once: true, amount: 0.25 }}>
+            <Swiper
+                effect={'coverflow'}
+                grabCursor={true}
+                centeredSlides={true}
+                loop={true}
+                slidesPerView={'auto'}
+                coverflowEffect={{
+                    rotate: 0,
+                    stretch: 0,
+                    modifier: 2,
+                }}
+                modules={[EffectCoverflow]}
+            >
+                {PORTFOLIO_IMAGES.map(({ src, alt }, index) => (
+                    <SwiperSlide key={index} className="lg:!w-96 !w-64 aspect-square">
+                        <Image className="object-cover w-full h-full" src={src} alt={alt} />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </motion.div>
     )
 }
 
