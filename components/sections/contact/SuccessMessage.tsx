1a379d1e23bfcf791b1a6aed41d90cadb38009d8
Pásztor Bence <pasztorbencee@gmai.com>
2023-11-09 15:51:58 +0100

    feat: change icon in SuccessMessage

--- components/sections/contact/SuccessMessage.tsx
+++ components/sections/contact/SuccessMessage.tsx
@@ -1,9 +1,9 @@
-import SuccessIcon from '@/components/sections/contact/SuccessIcon'
+import { CheckCircle2 } from "lucide-react"
 
 const SuccessMessage = () => {
     return (
         <div className="flex flex-col items-center text-center my-3" role="alert">
-            <SuccessIcon className="text-green-500 mb-3" />
+            <CheckCircle2 size={40} className="text-green-500 mb-3" />
             <h3 className="text-2xl font-medium mb-1">Sikeres ajánlatkérés!</h3>
             <p>Üzenetedet megkaptam, hamarosan felveszem veled a kapcsolatot.</p>
         </div>
